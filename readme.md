c099a6dbfded7a95508b684c26ff99ba36b8ae38
Yatin Bawa <yatinabc3@gmail.com>
2026-01-04 12:33:02 +0530

    Wrapping explained

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Learning Rust
+___
+
+In this repo, I will post all the things I am learning from rust book
+
+I am using the following [book]("https://rust-book.cs.brown.edu/ch03-03-how-functions-work.html") to learn. 
+
+Currently working on Part 3, Common Programming Concepts
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-fn main() {
-    let thesize: usize = 1303993;
-    println!("Size of USIZE is: {}", size_of_val(&thesize));
+use std::num::Wrapping;
 
-    let val = expression_example(12);
-    println!("The value is: {val}");
+fn main() {
+    // usize is dependent on
+    // operating system architecture
+    let the_size: usize = 1303993;
+    println!("Size of USIZE is: {}", size_of_val(&the_size));
+
+    let val = expression_example(1998);
+    println!("The Double of expression when truncated to i8 is: {val} and it's size is {}", size_of_val(&val));
 }
 
+// how expression in rust are different from other languages
+// while {} would be considered object in JS, in rust this just
+// returns the last expression
 fn expression_example(val: i32) -> i8 {
     {
-        let i8val: i8 = val.try_into().expect("Overflow");
+        // this might overflow, this is
+        // to understand sizes of variables
+        // let val: i8 = val.try_into().expect("The value has overflowed in expression example function");
+
+        // Wrapping results a tuple, wrapping.0 results in
+        // first item of the tuple, which is our Two's Compliment truncated item.
+        // a simple val as i8 would have also worked
+        // Wrapping function is just more explicit
+        // it provides addition, subtraction, multiplication
+        // for the value.
+
+        let i8val: i8 = (Wrapping(val as i8) + Wrapping(val as i8)).0;
         i8val
     }
 }
