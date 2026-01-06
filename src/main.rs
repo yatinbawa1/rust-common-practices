e44121654535c52abc5d46dcf691e7dba1f0d320
Yatin Bawa <yatinabc3@gmail.com>
2026-01-06 20:51:39 +0530

    implicit and explicit references

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-fn main() {
+// implicit and explicit reference conversion
+fn imp_exp_ref() {
+	let x = 5;
+	let y = &x;
+	let z = i32::abs(*y);
+	let k = y.abs();
 
+	println!("Implicit: {k}  Explicit: {z}");
+}
+
+fn main() {
+	imp_exp_ref();
 }
 
